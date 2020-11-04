772b0b4c4e7e3f4a18b2d2561259b64468d6d490
johnrobertmcc <john.robert.mcc@gmail.com>
2020-11-04 09:56:02 -0500

    added to entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,13 +1,15 @@
 import { combineReducers } from "redux";
 import NotebookReducer from "./notebook_reducer";
 import NotesReducer from "./note_reducer";
+import TagsReducer from "./tag_reducer";
 
 import usersReducer from "./user_reducer";
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
     notes : NotesReducer,
-    notebooks : NotebookReducer
+    notebooks : NotebookReducer,
+    tags: TagsReducer
 });
 
 export default entitiesReducer;
