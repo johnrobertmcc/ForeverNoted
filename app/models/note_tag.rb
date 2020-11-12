f4fa0c1e064816a1e070dd9bdc5451349bff8859
johnrobertmcc <john.robert.mcc@gmail.com>
2020-11-12 14:52:42 -0500

    added ntoes_wth_tags

--- app/models/note_tag.rb
+++
@@ -1,2 +0,0 @@
-class NoteTag < ApplicationRecord
-end
--- app/models/notebook.rb
+++ app/models/notebook.rb
@@ -4,5 +4,27 @@ class Notebook < ApplicationRecord
     has_many :notes
     belongs_to :user
 
+    # has_many :tags,
+        # through :notes,
+        # source :Note
+
+    def notes_with_tags
+        fin = []
+        
+        notes.each do |note|
+
+            tag = {}
+            
+            if note.tag
+                tag['id'] = note.tag.id
+                tag['note_id'] = note.id
+                fin << tag
+            end
+
+        end 
+
+        return fin
+    end
+
 end
 
